b0f65d7f75c7194026a702bc68a2c48e2d1b43b8
ANSAR-PANHWAR <panhwarking001@gmail.com>
2025-01-25 21:36:12 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -63,7 +63,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/JawadYTX/KHAN-DATA/raw/refs/heads/main/autovoice/repo.m4a' },
+            audio: { url: 'https://github.com/ANSAR-PANHWAR/PANHWAR-DATA/raw/refs/heads/main/autovoice/ansar.m4a' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
