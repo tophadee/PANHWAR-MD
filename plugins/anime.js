3942cc6eaf70a6359452d4f9a631ea7c8b28cfa6
ANSAR-PANHWAR <panhwarking001@gmail.com>
2025-01-25 19:06:20 +0500

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -12,13 +12,18 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 
 let dec = `❮❮❮ *PANHWAR-MD ANIME PHOTOS* ❯❯❯`
-await conn.sendMessage(from,{image:{url: `https://i.ibb.co/7NKFsxS/IMG-20241119-WA0087.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/51b44e4b086667361061b.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7d165d73f914985542537.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/3d9732d2657d2d72dc102.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/8daf7e432a646f3ebe7eb.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7514b18ea89da924e7496.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://i.ibb.co/7NKFsxS/IMG-20241119-WA0087.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/czqkh.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/ffEhL.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/QjmEp.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/ckrBb.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/zpLMu.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/NoLki.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/sSuRH.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/GBSxz.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/ttRck.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/hGpcx.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/mkKhq.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://qu.ax/zyzHa.jpg`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
